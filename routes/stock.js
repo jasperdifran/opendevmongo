e7a481d9547fffaecd06b387f0bb8703730b5285
jasperdifran <68628968+jasperdifran@users.noreply.github.com>
2022-09-29 15:27:20 +1000

    Add mongodb

--- routes/stock.js
+++ routes/stock.js
@@ -1,39 +1,26 @@
 const express = require('express')
-const db = require('../db.json')
 const fs = require('fs/promises')
+const db = require('../db')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    // Send back entire DB
-    res.send(db)
+router.get('/', async (req, res) => {
+    let items = []
+    let item = await db.getStockCollection().find().toArray()
+    res.send(item)
 })
 
-router.get('/:name', (req, res) => {
-    // Loop through DB
-    db.forEach(elem => {
-        // If name matches, send back that item
-        if (elem.name == req.params.name) {
-            res.send(elem)
-        }
-    });
+router.get('/:name', async (req, res) => {
+    let item = await db.getStockCollection().findOne({name: req.params.name})
+    res.send(item);
 })
 
 router.post('/additem', async (req, res) => {
-    // Read from DB
-    let dbData = await fs.readFile('./db.json')
-
-    // Convert to a format we can manipulate
-    let dbJson = JSON.parse(dbData)
-
-    // Add in new item
-    dbJson.push(req.body)
-
     // Write back to DB
-    await fs.writeFile('./db.json', JSON.stringify(dbJson))
+    await db.getStockCollection().insert(req.body);
 
     // Send response
-    res.send({"message": "Item added"})
+    res.send({message: "Item added"})
 })
 
 
